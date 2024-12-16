47f369dcdec69ca4863f5399d721d01a266d0c8d
ChrisJones <chris.jones@cmap.tech>
2024-12-16 11:53:14 +0000

    partially disabled popup notifs

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -3,7 +3,7 @@ return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     opts = {
-      transparent_mode = true,
+      transparent_mode = false,
     },
     init = function()
       vim.cmd("colorscheme gruvbox")
