011b34d14aef6b6d27270c4b5536c44f07a778b0
ChrisJones <chris.jones@cmap.tech>
2024-12-20 14:49:07 +0000

    build and test

--- lua/GTWelsh/remaps.lua
+++ lua/GTWelsh/remaps.lua
@@ -2,5 +2,6 @@ local map = function (mode, lhs, rhs)
     vim.keymap.set(mode, lhs, rhs, { nowait = true, silent = true })
 end
 
-map('n', '<leader>cb', '<cmd>!build.bat<CR>')
-map('n', '<leader>cB', '<cmd>!rebuild.bat<CR>')
+map('n', '<leader>cbb', '<cmd>!build.bat<CR>')
+map('n', '<leader>cbr', '<cmd>!rebuild.bat<CR>')
+map('n', '<leader>ctc', '<cmd>!dotnet test ./Source/*Tests/**/Debug/*Tests.dll --filter "FullyQualifiedName~%:t:r"<CR>')
